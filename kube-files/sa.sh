84e8a114c21722c37b3577ed208e2f34349de909
Sneha Mudavath <102925176+SnehaMudavath@users.noreply.github.com>
2022-08-13 21:33:26 -0400

    Automation of service account installation

---
+++ kube-files/sa.sh
@@ -0,0 +1,15 @@
+echo "Create IRSA Service Account" 
+#Get oidc
+eksctl utils associate-iam-oidc-provider --cluster group3 --approve
+set -x
+
+sleep 5
+
+echo "display oidc"
+aws eks describe-cluster --name  group3 --query cluster.identity.oidc.issuer --output text
+
+sleep 5
+
+echo "create service account with IAM role and policy attached"
+eksctl create iamserviceaccount     --name clo835     --namespace final     --cluster group3     --attach-policy-arn arn:aws:iam::616865343531:policy/s3-restricted-read     --approve     --override-existing-serviceaccounts;
+set -x
